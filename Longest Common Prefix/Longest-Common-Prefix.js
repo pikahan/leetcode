b6cbdb9e2c178b21725ea0d85b47e4eeb1fc9f0d
pikahan <yhl990104@gmail.com>
2018-08-16 21:53:33 +0800

    add Longest Common Prefix

---
+++ Longest Common Prefix/Longest-Common-Prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return ''
+    let ret = ''
+    let pos = 0
+    const len = Math.min(...strs.map(str => str.length))
+    let ch
+    while (pos < len) {
+        ch = strs[0].charAt(pos)
+        for (let i = 0; i < strs.length; i++) {
+            console.log(ch)
+            if (ch !== strs[i].charAt(pos)) return ret
+        }
+        ret += ch
+        pos++
+    }
+    return ret
+};
+
+console.log(longestCommonPrefix([]))
