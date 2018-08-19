3c1d0bfbdb3e475db56a58c3e09c02e0fe3717fa
pikahan <yhl990104@gmail.com>
2018-08-19 21:26:17 +0800

    optimise the arithmetic

--- arithmetic/Median of Two Sorted Arrays/Median-of-Two-Sorted-Arrays.js
+++ arithmetic/Median of Two Sorted Arrays/Median-of-Two-Sorted-Arrays.js
@@ -4,11 +4,45 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const newArr = [...nums1, ...nums2].sort((a,b) => {
-        return a - b
-    })
-    let len = newArr.length
-    const pos = parseInt(len/2)
-    if (len % 2) return newArr[pos]
-    return (newArr[pos-1] + newArr[pos]) / 2
+    let m = nums1.length
+    let n = nums2.length
+
+    // 使用解构赋值的话性能会差一点
+    if (m > n) {
+        [m, n] = [n, m]
+        ;[nums1, nums2] = [nums2, nums1]
+    }
+
+    let iMin = 0
+    let iMax = m
+    let mid_len = Math.floor((m + n + 1) / 2)
+    let min_right, max_left
+    while (iMin <= iMax) {
+        let i = Math.floor((iMin + iMax) / 2)
+        let j = mid_len - i
+        if (i !== m && nums2[j - 1] > nums1[i]) {
+            iMin = i + 1
+        } else if (i !== 0 && nums1[i - 1] > nums2[j]) {
+            iMax = i - 1
+        } else {
+            if (i === 0) {
+                max_left = nums2[j - 1]
+            } else if (j === 0) {
+                max_left = nums1[i - 1]
+            } else {
+                max_left = Math.max(nums1[i - 1], nums2[j - 1])
+            }
+
+            if ((m + n) % 2 === 1) return max_left
+
+            if (i === m) {
+                min_right = nums2[j]
+            } else if (j === n) {
+                min_right = nums1[i]
+            } else {
+                min_right = Math.min(nums1[i], nums2[j])
+            }
+            return (max_left + min_right) / 2
+        }
+    }
 };
