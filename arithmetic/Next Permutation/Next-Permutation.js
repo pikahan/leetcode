f3b6c272f9c05b990e7979691684f7933b05c1e7
pikahan <yhl990104@gmail.com>
2018-08-25 21:44:31 +0800

    optimize the algorithm

--- arithmetic/Next Permutation/Next-Permutation.js
+++ arithmetic/Next Permutation/Next-Permutation.js
@@ -4,34 +4,32 @@
  */
 var nextPermutation = function(nums) {
     let len = nums.length
-    let pos = len - 2
-    let temp
-    while (pos >= 0) {
-        let min = pos + 1
-        for (let j = pos+1; j < len; j++) {
-            if (nums[min] > nums[j] && nums[j] > nums[pos]) min = j
-        }
-        if (nums[min] > nums[pos]) {
-            temp = nums[min]
-            nums[min] = nums[pos]
-            nums[pos] = temp
-            sort(nums, pos+1)
-            return
-        }
-        pos--
+    let i = len - 2
+    while (i >= 0 && nums[i+1] <= nums[i]) {
+        i--
     }
-    sort(nums, 0)
+    if (i >= 0) {
+        let j = nums.length - 1
+        while (j >= 0 && nums[j] <= nums[i]) {
+            j--
+        }
+        swap(nums, i, j)
+    }
+    reverse(nums, i + 1)
 };
 
-function sort(arr, start) {
+function swap(arr, i, j) {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+function reverse(arr, start) {
     let temp
-    for (let i = start; i < arr.length-1; i++) {
-        let min = i
-        for (let j = i+1; j < arr.length; j++) {
-            if (arr[j] < arr[min]) min = j
-        }
-        temp = arr[i]
-        arr[i] = arr[min]
-        arr[min] = temp
+    let i = start, j = arr.length - 1
+    while (i < j) {
+        swap(arr, i, j)
+        i++
+        j--
     }
 }
